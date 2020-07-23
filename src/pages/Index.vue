59b1a79fa5464b4351f84dc3e2fbcc6921b233c9
Benriez <33022072+Benriez@users.noreply.github.com>
2020-07-23 11:26:22 +0200

    added q-input field and modified

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,34 @@
 <template>
   <q-page class="flex column">
+    <div class="col q-pt-lg q-px-md">
+      <q-input
+        bottom-slots
+        v-model="search"
+        placeholder="location"
+        dark
+        borderless=""
+        >
+        <template v-slot:before>
+          <q-icon name="my_location" />
+        </template>
 
+
+        <template v-slot:append>
+          <q-btn round dense flat icon="search" />
+        </template>
+      </q-input>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data(){
+    return{
+      search: ''
+    }
+  }
 }
 </script>
 
